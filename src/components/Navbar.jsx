a83cf199fe4dd870eb94f85f00ac6f82337c0d16
Carlos Basso <carlos.baso23@gmail.com>
2023-09-14 10:23:06 -0500

    almost done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import { BsChevronDown } from "react-icons/bs";
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [scroll, setScroll] = useState(true);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 200) {
+      setScroll(false);
+    } else {
+      setScroll(true);
+    }
+  };
+
+  if (typeof window !== "undefined") {
+    window.addEventListener("scroll", changeBackground);
+  }
+  return (
+    <div
+      class={
+        scroll
+          ? "w-full h-[70px] flex font-medium fixed z-50 transition-all bg-[#eeefe9]"
+          : "w-full h-[70px] flex font-medium fixed z-50 transition-all glass shadow-xl"
+      }
+    >
+      <div class="m-auto flex max-w-[1800px] w-full relative">
+        {/* LEFT CONTENT----------------------------------------------- */}
+        <div class="my-auto text-[#313d47] text-[min(6vw,30px)] absolute left-0 fixedCenterY sm:pl-10 pl-3 font-bold tracking-tight">
+          DOGUR
+        </div>
+        {/* HIDDEN AFTER XL----------------------------------------------- */}
+        <div class="xl:flex hidden">
+          {/* MIDDLE CONTENT----------------------------------------------- */}
+          <div class="flex text-[min(4.5vw,16.5px)] gap-20 text-[#313d47] absolute fixedCenterXnY">
+            <div class="my-auto cursor-pointer">Catalogue</div>
+            <div class="my-auto cursor-pointer">About</div>
+            <div class="my-auto cursor-pointer">Testimonials</div>
+          </div>
+
+          {/* RIGHT CONTENT----------------------------------------------- */}
+          <div class="flex text-[min(4.5vw,20px)] gap-[45px] absolute right-0 fixedCenterY sm:pr-10 pr-3">
+            <div class="my-auto flex text-[min(4.5vw,15px)]">
+              <div>USD</div>
+              <div>$</div>
+              {/* DOWN ICON----------------------------------------------- */}
+              <div class="my-auto ml-1">
+                <BsChevronDown />
+              </div>
+            </div>
+            {/* SEARCH ICON----------------------------------------------- */}
+            <div class="my-auto">
+              <BiSearch />
+            </div>
+            {/* BAG ICON----------------------------------------------- */}
+            <div class="my-auto">
+              <HiOutlineShoppingBag />
+            </div>
+            {/* LOGIN BUTTON----------------------------------------------- */}
+            <div class="text-[min(3.5vw,15px)] flex text-white">
+              <div class="bg-[#9959d0] py-2 px-7 rounded-full cursor-pointer">
+                Login
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,22 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			sm: "580px",
+			// => @media (min-width: 640px) { ... }
+	  
+			md: "868px",
+			// => @media (min-width: 768px) { ... }
+	  
+			lg: "1024px",
+			// => @media (min-width: 1024px) { ... }
+	  
+			xl: "1280px",
+			// => @media (min-width: 1280px) { ... }
+	  
+			"2xl": "1536px",
+			// => @media (min-width: 1536px) { ... }
+		  },
 		extend: {},
 	},
 	plugins: [],
