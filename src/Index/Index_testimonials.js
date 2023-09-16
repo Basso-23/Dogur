7e5e8d2d76b6437376baec3321d902c03592bb0d
Carlos Basso <carlos.baso23@gmail.com>
2023-09-16 12:21:30 -0500

    Add Indexs

---
+++ src/Index/Index_testimonials.js
@@ -0,0 +1,64 @@
+const testimonials = [
+  {
+    name: "Karly Baso1",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+  {
+    name: "Karly Baso2",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+  {
+    name: "Karly Baso3",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+  {
+    name: "Karly Baso4",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+  {
+    name: "Karly Baso5",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+  {
+    name: "Karly Baso6",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+  {
+    name: "Karly Baso7",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+  {
+    name: "Karly Baso8",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+  {
+    name: "Karly Baso9",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+  {
+    name: "Karly Baso10",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+  {
+    name: "Karly Baso11",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+  {
+    name: "Karly Baso12",
+    testimonial:
+      "I've never experienced such heavenly donuts! Dogur introduced me to a whole new world of flavors and made me a lifelong donu enthusiast.",
+  },
+];
+
+export { testimonials };
